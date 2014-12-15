b0242e39d96cebfe271ed62dfccd320aa5ee9214
cliffdurden <cliff@cliffs-MacBook-Pro.local>
2014-12-15 23:30:23 +0300

    Initial commit

---
+++ src/test/java/com/cliffdurden/AppIT.java
@@ -0,0 +1,12 @@
+package com.cliffdurden;
+
+import org.junit.Test;
+
+public class AppIT{
+
+    @Test
+    public void testIntegration(){
+        System.out.println("-== This is integration test ==-");
+    }
+
+}
---
+++ src/test/java/com/cliffdurden/AppTest.java
@@ -0,0 +1,11 @@
+package com.cliffdurden;
+
+import org.junit.Test;
+
+public class AppTest{
+
+    @Test
+    public void AppTest() {
+        System.out.println("-== This is unit-test ==-");
+    }
+}
